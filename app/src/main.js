daf8af4c625cc2f4be9dd301f8249744b9a32b66
Benjaminu10 <benjaminu10@nycstudents.net>
2025-12-19 21:14:24 -0500

    12/19/25 - Home1

--- app/src/main.js
+++ app/src/main.js
@@ -19,14 +19,14 @@ import './style.css';
 getData('pikachu'); */
 
 async function randomPoke() {
-    let randomId = Math.floor(Math.random() * 1350);
+    let randomId = Math.floor(Math.random() * 1025);
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
         if (!response.ok) {
             throw new Error(`Network response was not ok: ${response.status}`);
         } else {
             const data = await response.json();
-            console.log(data);
+            return data;
         }
     } catch (error) {
         console.error('Error fetching random Pokemon:', error);
@@ -35,10 +35,27 @@ async function randomPoke() {
 
 
 async function loadPoke() {
-    let currentpoke = randomPoke();
+    try {
+        const currentPoke = await randomPoke();
+        console.log(currentPoke);
 
-    console.log(currentpoke);
-    document.getElementById("pokeSprite").src = `${currentpoke.sprites.front_default}`;
-    console.log(currentpoke.sprites.front_default)
+        document.getElementById("pokeSprite").src =
+            currentPoke.sprites.front_default;
+
+    } catch (error) {
+        console.error('Error fetching random Pokemon:', error);
+    }
 }
+
+document.addEventListener("keydown", (event) => {
+    const start = document.getElementById("startScreen");
+    const game = document.getElementById("gameOptions")
+    if (event.key === "Enter" && !start.classList.contains("hidden")) {
+        start.classList.add("hidden");
+        game.classList.remove("hidden");
+        loadPoke();
+    }
+})
+
+
 
---
+++ app/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        pokemon: ['pokemonLogo', 'sans-serif'],
+        press: ['"Press Start 2P"', 'system-ui'],
+      },
+    },
+  },
+  plugins: [],
+}
